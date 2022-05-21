864568058d62290172784ef5b57bddcc1cf76b2a
unknown <khanvuthy88@gmail.com>
2022-05-21 14:22:31 +0700

    Remove website_event

--- ideaview/__manifest__.py
+++ ideaview/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'theme_ideaview', 'website_event'],
+    'depends': ['theme_ideaview'],
 
     # always loaded
     'data': [
